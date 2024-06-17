01fbb33c4b85c4ad6eb3a1facd76eca035df4880
mike <mixailted@gmail.com>
2024-06-18 01:51:14 +0400

    ADD:  component  Card

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,20 @@
 
 <script setup>
 import  AppHeader from './components/AppHeader.vue'
-
+import AppCard from './components/AppCard.vue';
 
 </script>
 
 <template>
-<div class=" bg-white w-4/5 m-auto h-screen rounded-xl shadow-xl mt-14 "> 
+<div class=" bg-white w-4/5 m-auto  rounded-xl shadow-xl mt-14 "> 
  
 <AppHeader/>
+<AppCard/>
+<AppCard/>
+<AppCard/>
+<AppCard/>
+<AppCard/>
+<AppCard/>
 
 </div>
 </template>
