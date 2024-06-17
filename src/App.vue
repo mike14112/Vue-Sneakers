97635b817080d5945cf41f9d15f09d56d235ec2e
mike <mixailted@gmail.com>
2024-06-18 01:32:48 +0400

    Add: Components & img for project

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,16 @@
+
 <script setup>
+import  AppHeader from './components/AppHeader.vue'
+
 
 </script>
 
 <template>
-<div class="text-red-500 w-full"> hello </div>
+<div class=" bg-white w-4/5 m-auto h-screen rounded-xl shadow-xl mt-14 "> 
+ 
+<AppHeader/>
+
+</div>
 </template>
 
 <style scoped>
