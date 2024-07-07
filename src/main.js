f00f665fe38959259cae4e473c397bbf6a32150c
mike <mixailted@gmail.com>
2024-07-07 16:07:50 +0400

    import: pages  import

--- src/main.js
+++ src/main.js
@@ -3,9 +3,13 @@ import './index.css'
 import { createApp } from 'vue'
 import { createRouter, createWebHistory } from 'vue-router'
 import App from './App.vue'
+import AppHome from './pages/AppHome.vue'
+import AppFavovites from '.pages/AppFavorites.vue'
+
+
 const routes = [
-    { path: '/', component: 'Home' },
-    { path: '/AppFavorites', component: 'Favorites' },
+    { path: '/', name: 'Home', component: AppHome },
+    { path: '/AppFavorites', name: 'Favorites', component: AppFavovites },
 
 
 ]
